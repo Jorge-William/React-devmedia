8c8f73a4b157c127bf03a61828341b6edf093e41
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-02-26 15:38:08 -0300

    State hook and event

--- state-and-event/src/componentes/PaginaInicial/index.js
+++ state-and-event/src/componentes/PaginaInicial/index.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import './estilo.css';
 
 export default function PaginaInicial() {
-  const [numeroAleatorio, setNumeroAleatorio] = useState(1);
+  const [numeroAleaorio, setNumeroAleatorio] = useState(1);
+
   const gerarNumero = () => {
-    const novoNumero = Math.floor(Math.random() * (100 - 1) + 1);
-    setNumeroAleatorio(novoNumero);
+    const novoNumero = Math.round(Math.random() * (100 - 1) + 1);
+    setNumeroAleatorio(novoNumero)
   }
+
   return (
     <div className="conteudo-centralizado">
       <h3>Número aleatório:</h3>
-      <h1>{numeroAleatorio}</h1>
+      <h1>{numeroAleaorio}</h1>
 
       <div className='area-botao'>
         <label>
           Click no botão abaixo para gerar um número aleatório:
           </label>
+
         <button onClick={gerarNumero}>
           Gerar número
           </button>
